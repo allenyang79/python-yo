3e90963d6c65f31ff6e680815076be7e7ba7db64
appier-allen <appier-user@jubing-macmini.local>
2013-12-23 15:29:30 +0800

    fix python  + yo

--- server/libs/utils.py
+++ server/libs/utils.py
@@ -1,59 +1,120 @@
 #utils for test
-import re
-def hello():
-	print "===HELLO==="
+import sys,os,re
+from flask import Flask,request,url_for,send_from_directory,current_app
+from werkzeug.exceptions import NotFound
 
-class Foo(object):
-	def __init__(self,name):
-		self.name=name
-	def sayHi(self):
-		print "hi.{}".format(self.name)
-
-class Bar(Foo):
-	def __init__(self):
-		print "Bar extend from Foo"
-		super(Bar, self).__init__("sub bar")
-	def sayHi(self):
-		super(Bar, self).sayHi()
-		print "Oops"
-
-
-class LiveReloadMiddleware(object):
+###
+#  a wsgi middleware that can inject lrSnippet in html
+###
+class Livereload_Middleware(object):
 	def __init__(self,app,path_regexp=r'\.(htm|html)$',lr_snippet='<!--LiveReload snippet-->'):
 		self.app=app
 		self.lr_snippet=lr_snippet
 		self.path_regexp=path_regexp
 	def __call__(self,environ,start_response):
-		print "[LiveReloadMiddleware]__call__"
-		print environ['PATH_INFO']
+		#print "[Livereload_Middleware]__call__"
+		#print environ
 		path_info=environ['PATH_INFO']
 		match = re.search(self.path_regexp, path_info)
 		if not match:
 			return self.app(environ,start_response)
 	
-		print "[LiveReloadMiddleware]==match=="
+		#print "[Livereload_Middleware]==match=="
 		start_response_args = []
 		#fake response output
 		def dummy_start_response(status, headers, exc_info=None):
-			print "[MyMid]dummy_start_response"
+			print "[Livereload_Middleware]dummy_start_response"
 			start_response_args.append(status)
 			start_response_args.append(headers)
 			start_response_args.append(exc_info)
 
 		#parse result
 		app_iter = self.app(environ,dummy_start_response)
+		if start_response_args[0][:3]!='200':
+			return self.app(environ,start_response)
+
 		output=""
 		for line in app_iter:
 			#output.write(line)
 			output+=line
 
 		output=re.sub("(<\/body>)",self.lr_snippet+"\\1",output)
+
 		#header and body
 		headers=[]
 		for name,value in start_response_args[1]:
 			if name.lower() != 'content-length':
 				headers.append((name.lower(),value))
 		headers.append(('Content-Length',str(len(output))))
+		#print "[Livereload_Middleware]"
 		start_response(start_response_args[0],headers,start_response_args[2])
 		return [output]
 
+
+###
+#  a wsgi middleware that will map static file to another folder
+###
+class Static_Assets_Middleware(object):
+	def __init__(self,app,oApp):
+		self.app=app
+		self.oApp=oApp
+	def __call__(self,environ,start_response):
+
+		start_response_args = []
+		#fake response output
+		def dummy_start_response(status, headers, exc_info=None):
+			print "[Static_Assets_Flask]dummy_start_response" 
+			start_response_args.append(status)
+			start_response_args.append(headers)
+			start_response_args.append(exc_info)
+
+		#fake start_response
+		self.app(environ,dummy_start_response)
+
+		#check static file
+		path_info=environ['PATH_INFO']
+		match = re.search(r'/static/(.*)', path_info)
+		if match.group(1):
+			filename=match.group(1)
+			if start_response_args[0][:3]=='404':
+				folders=['/Users/appier-user/Desktop/project/python/creative/static/app']
+				for folder in folders:
+					filepath="{}/{}".format(folder,filename)
+					if os.path.exists(filepath) and os.path.isfile(filepath):
+						pass
+						#print self.app
+						#print self.app.current_app
+						#print current_app
+						#print filename
+						#current_app.send_file(filepath)
+						#cache_timeout = self.app.get_send_file_max_age(filename)
+						#r=send_from_directory(folder, filename,cache_timeout=cache_timeout)
+						#print r
+		return self.app(environ,start_response)
+
+###
+#   map static folder with yeoman
+#   map static file to yeoman app folder or .tmp folder
+###
+class Static_Assets_Flask(Flask):
+	#overwrite static file mapping for develop
+	def send_static_file(self,filename):
+		print "[Static_Assets_Flask]send_static_file"
+
+		#scan below dictionary
+		if not self.has_static_folder:
+			return RuntimeError('No static folder for this object')
+		cache_timeout = self.get_send_file_max_age(filename)
+		folders=[]
+		if isinstance(self.static_folder,basestring):
+			folders.append(self.static_folder)
+		if isinstance(self.static_folder_mapping,(list,tuple)):
+			folders+=self.static_folder_mapping
+		for folder in folders:
+			filepath="{}/{}".format(folder,filename)
+			print filepath
+			if os.path.exists(filepath) and os.path.isfile(filepath):
+				r=send_from_directory(folder, filename,cache_timeout=cache_timeout)
+				print r
+				return r
+		raise NotFound()
--- server/main.py
+++ server/main.py
@@ -5,77 +5,51 @@ from werkzeug.serving import run_with_reloader
 from werkzeug.debug import DebuggedApplication
 from werkzeug.contrib.profiler import ProfilerMiddleware
 from werkzeug.exceptions import NotFound
-#from werkzeug.wrappers import Request,Response
-#from utils import Foo,Bar,hello
 
 libs_path = os.path.abspath('libs')
 sys.path.append(libs_path)
-import utils
-import livereload_middleware
+from utils import Static_Assets_Flask,Livereload_Middleware,Static_Assets_Middleware
 
-config={}
-config['debug']=True
-config['root_dir'] = os.path.dirname(os.path.abspath(__file__));
-config['static_app_dir'] = config['root_dir']+'/../static/app'
-config['static_dist_dir'] = config['root_dir']+'/../statc/dist'
-config['static_tmp_dir'] = config['root_dir']+'/../static/.tmp'
+#procress config 
+print sys.argv
+if len(sys.argv)==1:
+	config={}
+	config['debug']=True
+	config['root_dir'] = os.path.dirname(os.path.abspath(__file__));
+	config['static_folder']="static"
+	config['static_folder_mapping'] = [config['root_dir']+'/../static/app', config['root_dir']+'/../static/.tmp']
+elif len(sys.argv)>1:
+	sys.exit()
+	pass
 
-#map static folder with yeoman
-class MyFlask(Flask):
-	#overwrite static file mapping for develop
-	def send_static_file(self,filename):
-		#scan below dictionary
-		if not self.has_static_folder:
-			return RuntimeError('No static folder for this object')
-		cache_timeout = self.get_send_file_max_age(filename)
-		#scan static file
-		filepath="{}/{}".format(self.static_folder,filename)
-		if os.path.exists(filepath) and os.path.isfile(filepath):
-			return send_from_directory(self.static_folder, filename,cache_timeout=cache_timeout)
-		#scan app file
-		filepath="{}/{}".format(config['static_app_dir'],filename)
-		print filepath
-		if os.path.exists(filepath) and  os.path.isfile(filepath):
-			return send_from_directory(config['static_app_dir'], filename,cache_timeout=cache_timeout)
-		#scan .tmp file
-		filepath="{}/{}".format(config['static_tmp_dir'],filename)
-		if os.path.exists(filepath) and os.path.isfile(filepath):
-			return send_from_directory(config['static_tmp_dir'], filename,cache_timeout=cache_timeout)
-		raise NotFound()
+app=Static_Assets_Flask(__name__)
+app.static_folder=config['static_folder']
+app.static_folder_mapping=config['static_folder_mapping']
 
-	def make_response(self,rv):
-		print "[MyFlask]make_response"
-		return rv
-
-	def process_response(self,response):
-		response = super(MyFlask,self).process_response(response)
-		return response
-
-
-
-#end MyFlask
-app=MyFlask(__name__)
-
-#=end poit====
+#end point 
 @app.route('/')
-def hello_world():
+def index():
 	return 'this is index'
 
+@app.route('/foo')
+def foo():
+	return 'this is foo'
+@app.route('/bar')
+def bar():
+	return 'this is var'
 
-@app.route('/test_static')
-def test_static():
-	return url_for('static', filename='style.css')
 
-#=server run===
+
+#==run flask app===
 if __name__ == '__main__':
-
+	#flask run
 	#app.debug=True
 	#app.run()
 	#package app by middleWare
 
 	if config['debug']:
 		#debugMode
-		app=utils.LiveReloadMiddleware(app)
+		app=Livereload_Middleware(app)
 		wsgiApp= DebuggedApplication(app)
 		@run_with_reloader
 		def run_server():
@@ -87,4 +61,3 @@ if __name__ == '__main__':
 		#production
 		http_server=WSGIServer(('',5000),app)
 		http_server.serve_forever()
-
